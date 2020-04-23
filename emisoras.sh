43cd6a50f5de68cf59631e1fa6bd39dceaca2263
flopez <flopez@m2csolutions.com>
2020-04-23 11:36:11 +0200

    listen radio example

---
+++ emisoras.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#los 40 dance
+#mpv https://21233.live.streamtheworld.com/LOS40_DANCE_SC
+#cope
+#mpv https://net1-cope-rrcast.flumotion.com/cope/net1-low.mp3
+#Rock fm
+#mpv https://rockfm-cope-rrcast.flumotion.com/cope/rockfm-low.mp3
+#ser
+#mpv https://20863.live.streamtheworld.com/CADENASER_SC
+#megastar
+#https://megastar-cope.flumotion.com/chunks.m3u8
+
+los40Dance="https://21233.live.streamtheworld.com/LOS40_DANCE_SC"
+cope="https://net1-cope-rrcast.flumotion.com/cope/net1-low.mp3"
+rockFM="https://rockfm-cope-rrcast.flumotion.com/cope/rockfm-low.mp3"
+ser="https://20863.live.streamtheworld.com/CADENASER_SC"
+megastar="https://megastar-cope.flumotion.com/chunks.m3u8"
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Backtitle here"
+TITLE="Title here"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "los40Dance"
+         2 "cope"
+         3 "rockfm"
+         4 "ser"
+         5 "megastar")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "los40Dance"
+            mpv $los40Dance
+            ;;
+        2)
+            echo "cope"
+            mpv $cope
+            ;;
+        3)
+            echo "rockfm"
+            mpv $rockfm
+            ;;
+        4)
+            echo "ser"
+            mpv $ser
+            ;;
+        5)
+            echo "megastar"
+            mpv $megastar
+            ;;
+esac
